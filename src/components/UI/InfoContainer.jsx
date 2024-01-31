8e10ee55e20e90c164e9d5ab358e475b6e1650d9
EmilStanchev <emilstan4ev03@gmail.com>
2024-01-31 19:58:03 +0200

    Made contact form responsive

--- src/components/UI/InfoContainer.jsx
+++ src/components/UI/InfoContainer.jsx
@@ -2,9 +2,9 @@
 
 const InfoContainer = ({ title, description }) => {
   return (
-    <div className="mb-4">
+    <div className="mb-4 flex flex-wrap ">
       <h4 className="font-bold text-xl text-gray-500">{title}</h4>
-      <p>{description}</p>
+      <p className="flex flex-wrap flex-col">{description}</p>
     </div>
   );
 };
