07945be6a7d9a339e9d9f9b9e115a747daf487e0
EmilStanchev <emilstan4ev03@gmail.com>
2024-01-31 20:59:01 +0200

    Fixed the navbar bug

--- src/navigation/NavBar.jsx
+++ src/navigation/NavBar.jsx
@@ -44,7 +44,7 @@ const NavBar = () => {
         </div>
 
         {isMenuOpen && (
-          <div className="md:hidden fixed top-0 left-0 right-0 bottom-0 bg-white z-10">
+          <div className="lg:hidden fixed top-0 left-0 right-0 bottom-0 bg-white z-10">
             <div className="container mx-auto p-4">
               <div className="flex justify-end">
                 <button
